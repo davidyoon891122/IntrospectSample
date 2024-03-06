52eab02f8033e3e357488cb4ca2d40d212514e40
David Yoon <davidyoon@hanwha.com>
2024-03-06 11:22:00 +0900

    Initial Commit

---
+++ SwiftUIIntrospectSample/SwiftUIIntrospectSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIntrospectSampleApp.swift
+//  SwiftUIIntrospectSample
+//
+//  Created by Davidyoon on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIntrospectSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
